71d1ba8cf7f03a5ec721482a319dd3be6e6995a8
Llop Dieguez Luca Santino <l.llop@etec.local>
2023-04-20 12:29:38 -0300

    actualizacion mapa y movimiento camara

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform personaje;
+
+    private float tamañoCamara;
+    private float alturaPantalla; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tamañoCamara = Camera.main.ortographicSize;
+        //alturaPantalla = tamañoCamara * 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ypromedio = (personaje.position.y / 2);
+        Debug.Log(ypromedio);
+        Vector3 newPosition = new Vector3(personaje.position.x, personaje.position.y - ypromedio, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, 2f);
+    }
+}
