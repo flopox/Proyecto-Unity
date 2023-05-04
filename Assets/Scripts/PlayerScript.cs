fd6ffcd78f511f0c03e85f7cdd83c35e1350c191
Llop Dieguez Luca Santino <l.llop@etec.local>
2023-05-04 12:49:27 -0300

    movimiento tactil movile

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    bool isLeft = false;
+    bool isRight = false;
+
+    public Rigidbody2D rb;
+    public float speedForce;
+
+    public void clickLeft()
+    {
+        isLeft = true;
+    } 
+    public void releaseLeft()
+    {
+        isLeft = false;
+    } 
+    public void clickRight()
+    {
+        isRight = true;
+    }
+    public void releaseRight()
+    {
+        isRight = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isLeft) 
+        {
+            rb.AddForce(new Vector2(-speedForce, 0) * Time.deltaTime);
+        }
+        if (isRight)
+        {
+            rb.AddForce(new Vector2(speedForce, 0) * Time.deltaTime);
+        }
+    }
+}
